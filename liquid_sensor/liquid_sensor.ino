4fd4501212f10fb3e956a7c4f120f475e45c8719
Albert <44123868+Nanodayo23@users.noreply.github.com>
2019-03-22 20:43:38 +0100

    Add files via upload

---
+++ liquid_sensor/liquid_sensor.ino
@@ -0,0 +1,15 @@
+const int sensorPin= 0; //sensor pin connected to analog pin A0
+int liquid_level;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); //sets the baud rate for data transfer in bits/second
+pinMode(sensorPin, INPUT);//the liquid level sensor will be an input to the arduino
+}
+
+
+void loop() {
+  liquid_level= analogRead(sensorPin); //arduino reads the value from the liquid level sensor
+Serial.println(liquid_level);//prints out liquid level sensor reading
+delay(100);//delays 100ms
+  
+  }
---
+++ temperature_sensor/Temperaturoe.ino/Temperaturoe.ino.ino
@@ -0,0 +1,25 @@
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+void setup () {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin (9600);}
+void loop() {
+  // read the input on analog pin 0:
+  int value = analogRead(A0);
+  lcd.setCursor(0, 7);
+  if (value > 420) {
+    Serial.println("UWAGA! it's REALLY wet...");
+    lcd.print("Very heavy rain");}
+  else if ((value > 300) && (value <= 400)) {
+    Serial.println("Caution: it's wet...");
+//lcd.print("Average Rain");
+//lcd.print("       "); 
+}
+  else{
+    Serial.println("Don't worry it's not dry ;)");
+// lcd.print("Dry Wather");
+ //   lcd.print("          ");
+ }
+  delay(200);}
+
